8d9939038e4ba4fd14a2eccc7aedca01301983a7
BYRON LOARTE <by_tosh20@hotmail.com>
2024-09-28 08:54:31 -0500

    Update datosMemes.js

--- datosMemes.js
+++ datosMemes.js
@@ -1,1 +1,19 @@
+const obtenerMemes = async () => {
 
+    const memesContainers = document.getElementById('list-memes');
+    const memeTemplate = document.getElementById('meme'); 
+
+    const request = await fetch("https://api.imgflip.com/get_memes"); 
+
+    const respuesta = await request.json();
+
+    respuesta.data.memes.slice(0, 8).forEach(meme => {
+        const newMemeCard = memeTemplate.cloneNode(true);
+        const img = newMemeCard.querySelector('img');
+        img.src = meme.url; 
+        memesContainers.appendChild(newMemeCard);
+    });
+    memeTemplate.remove();
+};
+
+obtenerMemes();
